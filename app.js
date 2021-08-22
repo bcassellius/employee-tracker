3338366518cb925d25408cbc488dcd448d034e38
Bri <bhilliker@gmail.com>
2021-08-22 12:00:27 -0500

    fixed bugs in sql and seeds file and created psudo code for CLI

---
+++ app.js
@@ -0,0 +1,15 @@
+// use inquirer
+
+// ask user what they want to do -- modify employees, modify roles. or modify departments
+
+// based on their answer do the thing
+// you chose to work with employees. heres a list of alll the employees. what would u l;ike to do? add delete?
+
+// view/add/delete employees
+
+
+
+//once we get it working with employees, we can do these other ones
+// view/add/delete roles
+
+// view/add/delete departments
--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
+DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS departments;
 DROP TABLE IF EXISTS roles;
-DROP TABLE IF EXISTS employees;
 
 
 CREATE TABLE departments (
@@ -9,15 +9,15 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE roles (
-    id INTEGER PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL
+    salary DECIMAL,
     department_id INTEGER,
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
-    id INTEGER PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
